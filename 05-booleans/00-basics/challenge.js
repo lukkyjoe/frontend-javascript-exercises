b03f4f1507f08797c10e6be5bbf6fb4556a876ec
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-11 05:51:16 +0000

    boooooleans

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,29 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  var boolExp = stringOne == stringTwo
+  return boolExp
 };
 
 module.exports.notEqual = function(one, two) {
+  var result = one !== two;
+  return result
 
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  var result = (lower < middle) && (middle < upper)
+  return result
 };
 
 module.exports.outsideRanges = function(number) {
+//not between 10 and 20, it also can't be 10 or 20
+//not between 42 and 75, it also can't be 75. 42 is allowed
+//not between 1 and 6. 1 and 6 are allowed
+var result = (!(number >= 10 && number <= 20))&& (!(number > 42 && number <= 75)) && (!(number > 1 && number < 6))
+return result
 
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  var result = (name == 'NYTimes' || name == 'LATimes')&& (price >=1)
+  return result
 };
