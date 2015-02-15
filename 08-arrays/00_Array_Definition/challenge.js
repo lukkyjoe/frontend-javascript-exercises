6e1d82fd968b20fe01cc42bed7e714efbf654974
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-15 16:05:21 +0000

    arrays exercise upload 2

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(one,two,three,four){
+  return [one,two,three,four]
+};
 
-module.exports.firstAndLast = undefined;
+var originalArray = ['one',3,'cool',4]
+module.exports.firstAndLast = function(originalArray){
+  var lastElementIndex = originalArray.length - 1    
+  x = [originalArray[0],originalArray[lastElementIndex]]
+  return x
+};
