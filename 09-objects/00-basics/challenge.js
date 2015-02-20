534ac3cb778bdc3a67c4e3d39e3c499d1be37260
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-20 00:46:27 +0000

    object course struggles

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,31 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students){
+  var createCourse = {}
+  createCourse['title'] = title
+  createCourse['duration'] = duration
+  createCourse['students'] = students
+  return createCourse
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if (object.hasOwnProperty(newProp) == false){
+    object[newProp] = newValue
+  }
+  return object  
+  
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ',\n\n' + letter.msg + "\n\nSincerely,\n" + letter.sender
+}
 
-module.exports.canIGet = undefined;
+
+
+module.exports.canIGet = function(item, money){
+  var items = {}
+  items['MacBook Air'] = 999
+  items['MacBook Pro'] = 1299
+  items['Mac Pro'] = 2499
+  items['Apple Sticker'] = 1
+  return ((money >= items[item]) && items.hasOwnProperty(item))
+  
+};
