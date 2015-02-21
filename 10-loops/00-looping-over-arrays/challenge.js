458ced4581b088c66da7539d1fc51677286222b8
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-21 17:51:00 +0000

    not sure why output should be. check spec js

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+var number = 0
+for (var counter in array){
+  number += array[counter]
+}
+  return number
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString){
+  var lowString = inputString.toLowerCase()
+  return lowString.split(',')
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(inputArray){
+  var workArray = inputArray
+  for (var index in workArray){
+    workArray[index] = index + " is " + workArray[index]
+  }
+  return workArray
+};
