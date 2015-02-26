7bdae78e2c15bef23b990e3181bb60a034d46354
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-26 04:39:56 +0000

    while loops exercise

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,16 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionFn, actionFn){
+  while (conditionFn()){
+    actionFn()
+  }
+}
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(thisArray){
+  var counter = 0
+  var holdNumber = 0
+  while (counter < thisArray.length){
+    holdNumber = holdNumber + thisArray[counter]
+    counter++
+  }
+  return holdNumber
+}
 
