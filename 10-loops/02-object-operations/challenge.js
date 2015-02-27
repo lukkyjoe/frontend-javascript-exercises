64e7520131aff9fbb42fb796fab572fc3f88c504
lukkyjoe <joseph.sc.luk@gmail.com>
2015-02-27 00:30:14 +0000

    thursday night loop commit

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,30 @@
-module.exports.copy = undefined;
+module.exports.copy = function(theObject){
+  var secondObject = {}
+  for (index in theObject){
+    secondObject[index] = theObject[index]
+  }
+  return secondObject
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src){
+  var nowObject = dest
+  for (index in src){
+    nowObject[index] = src[index]
+  }
+  return nowObject
+}
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array){
+  var status = true
+  if (array.length == 0){
+    return status
+  }
+  for (var index in array){
+    console.log(index)
+    if (object.hasOwnProperty(array[index]) == false){
+    status = false};
+    }
+  
+  return status 
+}
+  
